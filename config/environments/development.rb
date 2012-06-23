453fcc5b083399bbfb2fea36a7550adae9ccdc4e
Doug Headley <headley.douglas@gmail.com>
2012-06-23 14:17:54 -0700

    Removed plural references to Snarker class.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Snarkers::Application.configure do
+Snarker::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
