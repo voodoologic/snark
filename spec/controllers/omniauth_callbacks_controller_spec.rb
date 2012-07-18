19ccc5e6915c85804516092532672bb0f3ff0c11
Doug Headley <headley.douglas@gmail.com>
2012-07-17 22:55:01 -0700

    Add first test for user Model.

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
+OmniAuth.config.test_mode = true
 describe OmniauthCallbacksController do
+
+  before do 
+    request.env["devise.mapping"] = Devise.mappings[:user] 
+    request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter] 
+  end
+
+  OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new( { 
+    provider:  'twitter', 
+    uid:  '123545', 
+    info: {nickname: "bosworth"}, 
+    credentials: {token: "1234554321TOKEN", secret: "6789009876SECRET"}
+  } )
+
+  it "should set the user" do 
+    get :twitter
+    assigns(:user).should_not be_nil
+  end
 
 end
